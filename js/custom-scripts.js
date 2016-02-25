1639b6c04f166a6cbf64fd2c1d162d9ccf573561
Martin Knoll Haakansson <bumstaerk@gmail.com>
2016-02-25 09:17:44 +0100

    Minor updates to the the top

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,1 +1,10 @@
 // For all the custom scripts on DAY
+$(document).ready(function(){
+
+  // Search Dropdown
+  $('.search-dropdown').on('click', function(e){
+    e.preventDefault();
+    $('#SearchBox').toggleClass('active');
+  });
+
+});
