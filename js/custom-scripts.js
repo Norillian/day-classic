b2806fa27b6abd7e183561a1ee5e4c6c7e14ae0d
JlobodaScancommerce <jlo@scancommerce.dk>
2016-02-26 15:05:55 +0200

    product Detail and Profile pages

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -1,6 +1,11 @@
 // For all the custom scripts on DAY
 $(document).ready(function(){
-
+  // Define Profile Page
+   if(window.location.href.indexOf("customer_edit") > -1) {
+       $('#ShopContent').addClass('profile-page');
+	   $('.profile-page').prepend("<h3 class='customEdit'>Edit profile</h3>");
+    }
+	
   // Search Dropdown
   $('.search-dropdown').on('click', function(e){
     e.preventDefault();
