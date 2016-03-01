c1f2dbb3afb11e802b9b829355386bc9db714182
Norillian <mic@scancommerce.dk>
2016-03-01 12:53:45 +0100

    CMS pages

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -5,26 +5,37 @@ $(document).ready(function(){
        $('#ShopContent').addClass('profile-page');
 	   $('.profile-page').prepend("<h3 class='customEdit'>Edit profile</h3>");
     }
-	
+
 	if(window.location.href.indexOf("AccountStatement") > -1) {
        $('#ShopContent').addClass('account-statement');
 	   $('.account-statement').prepend("<h3 class='customEdit'>See account statement</h3>");
     }
-	
+
 	if(window.location.href.indexOf("orderinvoicestat") > -1) {
        $('#ShopContent').addClass('order-page');
 	   $('.order-page').prepend("<h3 class='customEdit'>Order stat</h3>");
     }
-	
+
 	if(window.location.href.indexOf("Productlist.aspx") > -1) {
        $('#ShopContent').addClass('favourite-page');
 	   $('.favourite-page').prepend("<h3 class='customEdit'>Favourites list</h3>");
     }
-	
+
   // Search Dropdown
   $('.search-dropdown').on('click', function(e){
     e.preventDefault();
     $('#SearchBox').toggleClass('active');
+  });
+
+  // find carousels
+  $('.wide-banner.slider-content .cms-content-inner').slick({
+      dots: false,
+      infinite: true,
+      speed: 750,
+      fade: false,
+      autoplay: true,
+      arrows: false,
+      autoplaySpeed: 2500
   });
 
 });
