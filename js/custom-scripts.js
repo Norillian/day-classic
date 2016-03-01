7ffbe5a308f5859d3defb409fdc04d5164aff1ef
JlobodaScancommerce <jlo@scancommerce.dk>
2016-03-01 10:54:24 +0200

    Order and account pages

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -6,6 +6,21 @@ $(document).ready(function(){
 	   $('.profile-page').prepend("<h3 class='customEdit'>Edit profile</h3>");
     }
 	
+	if(window.location.href.indexOf("AccountStatement") > -1) {
+       $('#ShopContent').addClass('account-statement');
+	   $('.account-statement').prepend("<h3 class='customEdit'>See account statement</h3>");
+    }
+	
+	if(window.location.href.indexOf("orderinvoicestat") > -1) {
+       $('#ShopContent').addClass('order-page');
+	   $('.order-page').prepend("<h3 class='customEdit'>Order stat</h3>");
+    }
+	
+	if(window.location.href.indexOf("Productlist.aspx") > -1) {
+       $('#ShopContent').addClass('favourite-page');
+	   $('.favourite-page').prepend("<h3 class='customEdit'>Favourites list</h3>");
+    }
+	
   // Search Dropdown
   $('.search-dropdown').on('click', function(e){
     e.preventDefault();
