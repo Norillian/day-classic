ca8de29859e72beaa6fb0b5830dfa8c729da79d6
Norillian <mic@scancommerce.dk>
2016-03-03 11:19:43 +0100

    Batch of fixes and changes

--- js/custom-scripts.js
+++ js/custom-scripts.js
@@ -48,10 +48,11 @@ $(document).ready(function(){
   }
 
   //Remove specific links from productmenu
-  $('#pMenuSublevelsbdy a:contains("Never Out Of Stock")').remove();
-  $('#pMenuSublevelsbdy a:contains("DAY2DAY")').remove();
-  $('#pMenuSublevelsbdy a[href="/pl/Day-_16016.aspx"]').parent().nextAll('li').remove();
-  $('#pMenuSublevelsbdy a[href="/pl/Day-_16016.aspx"]').remove();
+  $('#pMenuSublevelsbdy a:contains("DAY Item")').parent().remove();
+  //$('#pMenuSublevelsbdy a:contains("Never Out Of Stock")').remove();
+  //$('#pMenuSublevelsbdy a:contains("DAY2DAY")').remove();
+  //$('#pMenuSublevelsbdy a[href="/pl/Day-_16016.aspx"]').parent().nextAll('li').remove();
+  //$('#pMenuSublevelsbdy a[href="/pl/Day-_16016.aspx"]').remove();
   //$('#pMenuSublevelsbdy a[href="/pl/2NDDAY-_16026.aspx"]').parent().nextAll('li').remove();
   //$('#pMenuSublevelsbdy a[href="/pl/2NDDAY-_16026.aspx"]').remove();
 
